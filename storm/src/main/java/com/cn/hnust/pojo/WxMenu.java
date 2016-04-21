bc9d70eb87a52637c4f929a4a8177a2220b457c6
admin <admin@DESKTOP-OL9ET3R>
2016-04-21 18:36:48 +0800

    。。

---
+++ storm/src/main/java/com/cn/hnust/pojo/WxMenu.java
@@ -0,0 +1,73 @@
+package com.cn.hnust.pojo;
+
+public class WxMenu {
+    private Long id;
+
+    private String name;
+
+    private String xkey;
+
+    private String type;
+
+    private String url;
+
+    private String mediaId;
+
+    private String parent;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getXkey() {
+        return xkey;
+    }
+
+    public void setXkey(String xkey) {
+        this.xkey = xkey == null ? null : xkey.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public String getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        this.mediaId = mediaId == null ? null : mediaId.trim();
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent == null ? null : parent.trim();
+    }
+}
